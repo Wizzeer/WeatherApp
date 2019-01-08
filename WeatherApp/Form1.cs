13e9a04310b512b2f9c2db1b260b483ab7a6e2d5
Wojtek <skrzynia777@gmail.com>
2019-01-08 21:12:13 +0100

    Add files via upload

--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -7,24 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace WeatherApp
 {
 	public partial class Form1 : Form
 	{
+        const string API_KEY = "294b51a2e5a87020ef4c90fd11502129";
+        string city = "";
+
 		public Form1()
 		{
 			InitializeComponent();
-		}
+            getWeather("Krakow");
+	 	}
 
-		private void label1_Click(object sender, EventArgs e)
-		{
+        void getWeather(string city)
+        {
+            WebClient web = new WebClient();
+            string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",city,API_KEY);
+            var json = web.DownloadString(url);
+            var result = JsonConvert.DeserializeObject<Pogoda>(json);
+            Pogoda output = result;
 
-		}
+            
 
-		private void label2_Click(object sender, EventArgs e)
-		{
 
-		}
+        }
 	}
 }
