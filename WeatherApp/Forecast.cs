37ed20b6f57d4ad45d559a73576c17cee3503b2d
Wojciech Skrzyński <wojciech.skrzynski@microsoft.wsei.edu.pl>
2019-01-09 22:14:36 +0100

    Updating backend

---
+++ WeatherApp/Forecast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class Forecast
+    {
+
+    }
+
+    public class list
+    {
+        public double dt { get; set; }
+        public city city { get; set; }
+        public temp temp { get; set; }
+    }
+
+   
+    public class temp
+    {
+        public int value { get; set; }
+    }
+
+    public class city
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+    }
+
+
+}
--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -10,30 +10,70 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 
+
 namespace WeatherApp
 {
 	public partial class Form1 : Form
 	{
         const string API_KEY = "294b51a2e5a87020ef4c90fd11502129";
         string city = "";
-
+        
 		public Form1()
 		{
 			InitializeComponent();
             getWeather("Krakow");
 	 	}
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
         void getWeather(string city)
         {
             WebClient web = new WebClient();
             string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",city,API_KEY);
             var json = web.DownloadString(url);
-            var result = JsonConvert.DeserializeObject<Pogoda>(json);
-            Pogoda output = result;
+            var result = JsonConvert.DeserializeObject<root>(json);
+            root output = result;
 
+            label1.Text = string.Format("{0}",output.name);
+            label5.Text = string.Format("{0}\u2103", output.main.temperature);
+            label6.Text = string.Format("Wilgotność: {0}%", output.main.humidity);
+            label2.Text = string.Format("{0}", output.sys.country);
             
 
 
         }
-	}
+
+        
+    }
+
+
+
+    public class main {
+
+        public int temperature{get;set;}
+        public int humidity{get;set;}
+    }
+
+    public class root
+    {
+        public string name { get; set; }
+        public main main { get; set; }
+        public sys sys { get; set; }
+    }
+
+    public class sys
+    {
+        public string country { get; set; }
+    }
+
+
+
 }
